80f3e8091e893529c4eb6f2f6c506b7eb63f5772
sahil <sahilpatel443385@gmail.com>
2023-04-11 21:38:09 +0530

    Practice

---
+++ 2_wp.js
@@ -0,0 +1,10 @@
+document.write("Vidhi Patel");
+
+//not complusory to write window
+window.alert("This is alert");
+// alert("This is alert1");
+window.confirm("This is confirm");
+// confirm("This is confirm1");
+window.prompt("This is prompt");
+
+Window.print()
